c1beb0afd4c5d7f768964ee3650182367b0a43a2
Arun K Biju <168078825+ArunKBiju@users.noreply.github.com>
2025-04-15 16:45:29 +0530

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://sbwswkusqejfocplxqvg.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNid3N3a3VzcWVqZm9jcGx4cXZnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM2ODc2NzMsImV4cCI6MjA1OTI2MzY3M30.15aLRmhiArT04IJOmubGP4isxzYM_jVvz9DdZBCYhQ0";
+const supabaseUrl = "valla panikkum poda";
+const supabaseKey ="naanam undo vallavantem code adichu maattaan";
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
